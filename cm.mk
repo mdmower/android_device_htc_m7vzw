ef61c6d709c6ccafa25014b3970c31afce22bb8c
Matt Mower <mowerm@gmail.com>
2014-02-14 00:59:00 -0600

    m7vzw: move device config to universal m7 tree
    Change-Id: I076b8ee2bf410cfa7b4c2f77aae67767c645ad84

--- cm.mk
+++ cm.mk
@@ -1,16 +1,3 @@
-$(call inherit-product, device/htc/m7vzw/full_m7vzw.mk)
-
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=HTCOneVZW \
-    BUILD_ID=JSS15J \
-    BUILD_FINGERPRINT="VERIZON/HTCOneVZW/m7wlv:4.3/JSS15J/278010.1:user/release-keys" \
-    PRIVATE_BUILD_DESC="2.10.605.1 CL278010 release-keys"
-
-PRODUCT_GMS_CLIENTID_BASE := android-verizon
-
-PRODUCT_NAME := cm_m7vzw
+$(call inherit-product, device/htc/m7/cm.mk)
 PRODUCT_DEVICE := m7vzw
+PRODUCT_NAME := cm_m7vzw
